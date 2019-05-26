11ddd52a1408061ff443c32fa547df4ea11e802a
Patrice Ricard <patricericard@cannaramacbookpro.local>
2019-05-25 23:07:43 -0400

    firebase creds

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,13 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: 'AIzaSyBJJ8I3VBoX8nue3TGRRLVkKCTGyu2qi3w',
+  authDomain: 'epicerie-mai-2019.firebaseapp.com',
+  databaseURL: 'https://epicerie-mai-2019.firebaseio.com',
+  projectId: 'epicerie-mai-2019',
+  storageBucket: 'epicerie-mai-2019.appspot.com',
+  messagingSenderId: '358081810881',
+  appId: '1:358081810881:web:55b38f3a3b633dd4'
 }
 
 firebase.initializeApp(config)
