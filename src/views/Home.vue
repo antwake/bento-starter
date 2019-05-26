fffc8d92d362390f7f8b338729a44a495a0862ed
Patrice Ricard <patricericard@cannaramacbookpro.local>
2019-05-25 23:10:05 -0400

    Add article in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,31 @@
 <template>
   <div class="page-wrapper">
-    <h1 class="home-page-title">{{ appTitle }}</h1>
-    <img alt="logo-bento" class="logo" src="@/assets/img/bento-starter.svg" />
-
-    <a
-      rel="noopener"
-      class="documentation-link"
-      target="_blank"
-      href="https://bento-starter.netlify.com/"
-      >Documentation →</a
-    >
+    <h1 class="articles-page-title">Articles page</h1>
+    <article-list class="article-list"></article-list>
+    <add-article v-if="networkOnLine"></add-article>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import ArticleList from '@/components/ArticleList'
+import AddArticle from '@/components/AddArticle'
 
 export default {
-  head: {
-    title: {
-      inner: 'Home'
-    },
-    meta: [
-      {
-        name: 'description',
-        content: 'Bento-starter home page',
-        id: 'desc'
-      }
-    ]
-  },
-  computed: mapState('app', ['appTitle'])
+  components: { ArticleList, AddArticle },
+  computed: mapState('app', ['networkOnLine'])
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@/theme/variables.scss';
 
-.page-wrapper {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+.articles-page-title {
+  text-align: center;
+  margin-bottom: 60px;
+}
 
-  .logo {
-    margin-bottom: 3rem;
-  }
-
-  .home-page-title {
-    text-align: center;
-  }
-
-  .documentation-link {
-    display: inline-block;
-    font-size: 1.2rem;
-    color: #fff;
-    background-color: #5d6788;
-    padding: 0.8rem 1.6rem;
-    border-radius: 4px;
-    transition: background-color 0.1s ease;
-    box-sizing: border-box;
-    text-decoration: none;
-    width: fit-content;
-    font-weight: 500;
-  }
+.article-list {
+  margin: 20px 0;
 }
 </style>
